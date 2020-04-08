1fc9e00f7379dbe25decc0466b89742e77918890
Sara Mohamed <sarasiraj2009@gmail.com>
2020-04-08 16:02:35 +0100

    sara

---
+++ app.py
@@ -0,0 +1,15 @@
+from application import app
+from flask import request, Response
+from random import randit
+
+@app.route(/random/color, methods = ['GET'])
+def random_color():
+    colors = ['red', 'yellow', 'green', 'orange']
+    color = colors[randit(0,30)]
+    return Response(color,mimetype='test/plain')
+
+
+@app.route(/random/number, methods = ['GET'])
+def random_number():
+    return Response(randit(1,8), mimetype='test/plain')
+
